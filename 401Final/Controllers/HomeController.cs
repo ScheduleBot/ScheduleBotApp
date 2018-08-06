81ff71beee4d4c0f42d7913611a5a8c958245d54
cascadianrebel <Anthonydgreen90@gmail.com>
2018-08-06 10:47:00 -0700

    Add project files.

---
+++ 401Final/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _401Final.Models;
+
+namespace _401Final.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RecentActivity()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult ItemList()
+        {
+            ViewData["Message"] = "This will display the items the bot can read.";
+
+            return View();
+        }
+
+        public IActionResult TestBot()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
