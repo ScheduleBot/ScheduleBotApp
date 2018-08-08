c785bf44d68c4b82c3d1352038f6335bc8c367dc
Collin Hintzke <collinhintzke5@gmail.com>
2018-08-08 14:50:00 -0700

    Styled front page

--- 401Final/Controllers/HomeController.cs
+++ 401Final/Controllers/HomeController.cs
@@ -5,15 +5,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Final401.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 
 namespace Final401.Controllers
 {
+
     [ApiExplorerSettings(IgnoreApi = true)]
     public class HomeController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration Configuration;
+
+        public HomeController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            Configuration = configuration;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Login", "Account");
         }
 
         public IActionResult RecentActivity()
